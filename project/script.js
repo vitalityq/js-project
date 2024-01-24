228e7cc7e792e8077c51397960f7db5278e68bcf
vc-webteam <v.clicev@webteam.studio>
2024-01-24 13:27:07 +0200

    first commit

--- project/script.js
+++ project/script.js
@@ -23,3 +23,25 @@
 
 'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.lenght < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+    }else {
+        console.log('error');
+        i--;
+    }
+}
