421ca8ac49085833430acbba50a3325dc3b9f946
vc-webteam <v.clicev@webteam.studio>
2024-01-24 16:46:48 +0200

    add if

--- project/script.js
+++ project/script.js
@@ -37,11 +37,23 @@ for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.lenght < 50) {
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
     personalMovieDB.movies[a] = b;
     console.log('done');
-    }else {
+    } else {
         console.log('error');
         i--;
     }
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка!")
+}
+
+console.log(personalMovieDB);
