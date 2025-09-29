952b70305b9535d828bf2fe3709b022dd829a301
Vitali C <40894756+vitalityq@users.noreply.github.com>
2025-09-29 11:36:10 +0300

    changedd to default

--- project/for lessons.js
+++ project/for lessons.js
@@ -1,18 +1,2 @@
-"use strict"
 
-// function showFirstMessage(text){
-//     console.log(text);
-// }
-
-// showFirstMessage("Hello world!");
-
-const usdCurr = 18;
-const eurCurr = 20;
-
-function convert(amount, curr){
-    console.log(curr * amount);
-}
-
-convert(50, usdCurr);
-convert(100, eurCurr);
 
